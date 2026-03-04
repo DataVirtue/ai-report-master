db91abb3c8012ec624d6a591e5296ee3b56cfa4f
Akshay Vikas Pawar <akshaypawar97@gmail.com>
2026-03-04 23:01:11 +0530

    added semantic model analyzer

--- ai_report_master/schema_introspector/main.py
+++ ai_report_master/schema_introspector/main.py
@@ -1,10 +1,16 @@
 from schema_introspector import SchemaIntrospector
+from semantic_model_analyzer import SemanticModelAnalyzer
 
 
 def main():
     introspector = SchemaIntrospector()
+    modelAnalyzer = SemanticModelAnalyzer()
+
     schema_dict = introspector.get_schema()
     print(schema_dict["data"][220])
+    schema_analysis = modelAnalyzer.generate_table_stats(schema_dict)
+    print("*" * 1000)
+    print(schema_analysis[220])
 
 
 if __name__ == "__main__":
---
+++ ai_report_master/schema_introspector/semantic_model_analyzer.py
@@ -0,0 +1,37 @@
+from typing import Dict, List
+
+
+class SemanticModelAnalyzer:
+    def _generate_count_stats(self, columns: List) -> Dict:
+        numeric_count = 0
+        fk_count = 0
+        date_count = 0
+        text_count = 0
+        for col in columns:
+            if col["semantic_type"] == "numeric":
+                numeric_count += 1
+            if col["semantic_type"] == "datetime":
+                date_count += 1
+            if col["semantic_type"] == "text":
+                text_count += 1
+            if col["is_foreign_key"]:
+                fk_count += 1
+
+        return {
+            "numeric_columns_count": numeric_count,
+            "date_column_count": date_count,
+            "text_column_count": text_count,
+            "foreign_key_column_count": fk_count,
+        }
+
+    def generate_table_stats(self, schema_dict: Dict) -> List:
+        table_stats_list = []
+        schema_data = schema_dict["data"]
+        for table_dict in schema_data:
+            table_stats_list.append(
+                {
+                    "table_name": table_dict["table_name"],
+                    **self._generate_count_stats(table_dict["columns"]),
+                }
+            )
+        return table_stats_list
