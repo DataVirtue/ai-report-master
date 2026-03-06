034fc8fb9dfd7762dd3ab6992c19205777e6b40e
Akshay Vikas Pawar <akshaypawar97@gmail.com>
2026-03-06 14:02:42 +0530

    created embedding generator starter

---
+++ ai_report_master/schema_introspector/embedding_generator.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class EmbeddingGenerator:
+
+    def __init__(self,embedding_model= ) -> None:
+        pass
+
+    def embed_text(self,text:str):
+        pass
+    
+    def embed_batch(self,texts:List[str]):
+        pass
+
+
