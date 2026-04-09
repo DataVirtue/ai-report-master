578b6d2d02a1cf9a8ad1e596be7820061a60e9bf
Akshay Vikas Pawar <akshaypawar97@gmail.com>
2026-04-09 15:44:34 +0530

    changed secrets to env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,11 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-22!__bxb!t@el*n0q0qmr9f&9*yi*t5edjsxdcl@gsx)2o#%0b"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = [
     os.getenv("ALLOWED_HOST"),
