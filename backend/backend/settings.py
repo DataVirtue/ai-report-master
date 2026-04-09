812e9e71ec9c7d255d0e4e5095166f23e1aa3ccb
Akshay Vikas Pawar <akshaypawar97@gmail.com>
2026-04-09 21:08:12 +0530

    fixed settings removed error issue

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,8 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-if not SECRET_KEY:
-    raise ImproperlyConfigured("SECRET_KEY environment variable is required")
 
 DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
 
